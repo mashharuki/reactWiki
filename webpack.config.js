682060755c0f5c8667576d6474c8d53f1ef43cc2
haruki kondo <avp.104.106.107@ae.auone-net.jp>
2021-07-18 10:57:51 +0900

    Webpack設定ファイルを追加しました。

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+/**
+ * Webpack用の設定ファイル
+ */
+
+// 必要なモジュールをインポートする。
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
