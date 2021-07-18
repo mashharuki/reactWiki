c8674857476740eb367c826e738d7ae5728c78ae
haruki kondo <avp.104.106.107@ae.auone-net.jp>
2021-07-18 11:46:31 +0900

    画面表示用のコンポーネントファイルを追加しました。

---
+++ src/wiki_show.js
@@ -0,0 +1,70 @@
+/**
+ * Wiki画面表示用コンポーネント定義ファイル
+ */
+
+// 必要なモジュールを読み込む
+import React from 'react';
+import request from 'superagent';
+import WikiParser from './wiki_parser';
+import styles from './styles';
+
+/**
+ * 画面表示コンポーネント
+ */
+export default class WikiEdit extends Component {
+    // コンストラクター関数
+    constructor (props) {
+        super(props);
+        const {params} = this.props.match;
+        // ステート変数を定義する。
+        this.state = {
+            name: params.name,
+            body: '',
+            loaded: false
+        };
+    }
+
+    // Wikiの内容を読み込む関数
+    componentWillMount () {
+        request
+            .get(`/api/get/${this.state.name}`)
+            .end((err, res) => {
+                if (err) return
+                this.setState({
+                    body: res.body.data.body,
+                    loaded: true
+                })
+            })
+    }
+
+    // Wiki記法をReactオブジェクトに変換する関数
+    convertText (src) {
+        // Wiki記法をパースして配列データに変換
+        const nodes = WikiParser.parse(src)
+        // 各要素をReactの要素に変換
+        const lines = nodes.map((e, i) => {
+            // リスト
+            if (e.tag === 'ul') {
+                const lis = e.items.map(
+                    (s, j) => <li key={`node${i}_${j}`}>{s}</li>
+                )
+                return <ul key={`node${i}`}>{lis}</ul>
+            }
+            if (e.tag === 'a') {
+                return (
+                    <div key={`node${i}`}>
+                        <a href={`/wiki/${e.label}`}>→{e.label}</a>
+                    </div>
+                )
+            }
+            return React.createElement(
+                e.tag, {key: 'node' + i}, e.label)
+            }
+        );
+        // 変換したオブジェクトを返す。
+        return lines;
+    }
+}
+
+// コンポーネントを外部に公開する。
+export default WikiShow;
