f9f3fa2f8539dd08ba518973b85ecd8d232233db
haruki kondo <avp.104.106.107@ae.auone-net.jp>
2021-07-18 11:22:41 +0900

    スタイル用のファイルを追加しました。

---
+++ src/style.js
@@ -0,0 +1,19 @@
+/**
+ * スタイル定義用ファイル
+ */
+const styles = {
+  show: {
+    border: '1px solid gray',
+    padding: 12
+  },
+  edit: {
+    padding: 12,
+    backgroundColor: 'silver'
+  },
+  right: {
+    textAlign: 'right'
+  }
+}
+
+// スタイルコンポーネントを外部に公開する。
+export default styles;
